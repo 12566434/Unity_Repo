f330bd45d40bd00ac880a6c7e166b612b81868ce
Ultimate Combatmaster <zainsalimuddin@gmail.com>
2021-08-30 01:18:38 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,10 +6,13 @@ public class PrintAndHide : MonoBehaviour
 {
     public int i;
     public Renderer rend;
+    public int randint;
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+
+        randint = Random.Range(200,251);
     }
 
     // Update is called once per frame
@@ -17,5 +20,11 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         Debug.Log(gameObject.name + ":" + i);
+        if (gameObject.CompareTag("Red") && (i == 100)) {
+            gameObject.SetActive(false);
+        }
+        else if(gameObject.CompareTag("Blue") && (i == randint)) {
+            rend.enabled = false;
+        }
     }
 }
